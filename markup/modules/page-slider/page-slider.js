f42c31c5f06c2892ab1e3380277240735279b575
Filipp Gaponenko <psr1919plus21@gmail.com>
2016-04-25 21:44:00 +0300

    :rocket: Gradually dark overlay next slide.

--- markup/modules/page-slider/page-slider.js
+++ markup/modules/page-slider/page-slider.js
@@ -17,26 +17,51 @@ _.forEach(slides, (slide) => {
 // Scroll slides.
 let currentSlide = 1;
 let pageYOld = window.pageYOffset;
+
 let scrollSlides = () => {
     let currentSlideBoundary = currentSlide * viewPortHeight;
+    let pageYNew = window.pageYOffset;
 
     // Scroll direction top.
-    if (pageYOld > window.pageYOffset && window.pageYOffset < currentSlideBoundary) {
-        slides[currentSlide].style.position = 'fixed';
-        slides[currentSlide].style.top = 'initial';
-        if (--currentSlide < 1) {
-            currentSlide = 1;
+    if (pageYOld > pageYNew) {
+
+        // Calculate nextSlide opacity while scroll up.
+        let nextSlideOpacity = 1 - (pageYNew / viewPortHeight - (currentSlide - 1));
+        let nextSlideOverlay = slides[currentSlide].querySelector('.detached-screen_overlay');
+        nextSlideOverlay.style.opacity = nextSlideOpacity;
+
+        // Set current slide fixed.
+        if (pageYNew < currentSlideBoundary) {
+            slides[currentSlide].style.position = 'fixed';
+            slides[currentSlide].style.top = 'initial';
+
+            // Switch slide when upslide overlay it.
+            if (pageYNew < (currentSlideBoundary - viewPortHeight) && --currentSlide < 1) {
+                currentSlide = 1;
+            }
         }
 
     // Scroll direction bottom.
-    } else if (window.pageYOffset >= currentSlideBoundary) {
-        slides[currentSlide].style.position = 'absolute';
-        slides[currentSlide].style.top = currentSlideBoundary + 'px';
-        if (currentSlide < (slides.length - 1)) {
-            currentSlide++;
+    } else {
+
+        // Calculate nextSlide opacity while scroll down.
+        let nextSlideOpacity = 1 - (pageYNew / viewPortHeight - (currentSlide - 1));
+        let nextSlideOverlay = slides[currentSlide].querySelector('.detached-screen_overlay');
+        nextSlideOverlay.style.opacity = nextSlideOpacity;
+
+        // Make current slide scrollable.
+        if (pageYNew >= currentSlideBoundary) {
+            slides[currentSlide].style.position = 'absolute';
+            slides[currentSlide].style.top = currentSlideBoundary + 'px';
+
+            // Switch to next slide.
+            if (currentSlide < (slides.length - 1)) {
+                currentSlide++;
+            }
         }
     }
-    pageYOld = window.pageYOffset;
+
+    pageYOld = pageYNew;
 };
 scrollSlides = _.throttle(scrollSlides, 10);
 
