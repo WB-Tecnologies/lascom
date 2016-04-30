2a981a5c96533d69751b61d9f0d3e4a5005a810b
Filipp Gaponenko <psr1919plus21@gmail.com>
2016-04-30 23:19:31 +0300

    :rocket: Machine page header.

--- markup/modules/header/header.js
+++ markup/modules/header/header.js
@@ -1,21 +1,28 @@
 import _ from 'lodash';
 
 const header = document.getElementsByClassName('header')[0];
-const headerFixed = document.getElementsByClassName('header-fixed')[0];
-const headerHeight = header.clientHeight;
 
-// Toggle header fixed/static position.
-let setHeader = () => {
-    if (window.pageYOffset > headerHeight) {
-        if (!headerFixed.classList.contains('header-fixed__active')) {
-            headerFixed.classList.add('header-fixed__active');
-        }
-    } else {
-        if (headerFixed.classList.contains('header-fixed__active')) {
-            headerFixed.classList.remove('header-fixed__active');
-        }
+fixedHeaderInit();
+
+function fixedHeaderInit() {
+    if (header) {
+        const headerFixed = document.getElementsByClassName('header-fixed')[0];
+        const headerHeight = header.clientHeight;
+
+        // Toggle header fixed/static position.
+        let setHeader = () => {
+            if (window.pageYOffset > headerHeight) {
+                if (!headerFixed.classList.contains('header-fixed__active')) {
+                    headerFixed.classList.add('header-fixed__active');
+                }
+            } else {
+                if (headerFixed.classList.contains('header-fixed__active')) {
+                    headerFixed.classList.remove('header-fixed__active');
+                }
+            }
+        };
+        setHeader = _.throttle(setHeader, 200);
+
+        window.addEventListener('scroll', setHeader);
     }
-};
-setHeader = _.throttle(setHeader, 200);
-
-window.addEventListener('scroll', setHeader);
+}
