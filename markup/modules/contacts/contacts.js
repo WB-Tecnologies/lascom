7c8cc51f4ee9f33d0ea6f1138b3c339fa772a588
Filipp Gaponenko <psr1919plus21@gmail.com>
2016-04-30 16:01:04 +0300

    :wrench: Run goggle-map via npm.

--- markup/modules/contacts/contacts.js
+++ markup/modules/contacts/contacts.js
@@ -1,1 +1,21 @@
+import googleApi from 'google-maps';
 
+googleApi.load((google) => {
+    initMap(google);
+});
+
+function initMap(google) {
+    let lascomOffice = {lat: 56.749565, lng: 37.226751};
+
+    let mapDiv = document.getElementById('map');
+    let map = new google.maps.Map(mapDiv, {
+        center: lascomOffice,
+        zoom: 15
+    });
+
+    let marker = new google.maps.Marker({
+        position: lascomOffice,
+        map: map,
+        title: 'Lascom office'
+    });
+}
