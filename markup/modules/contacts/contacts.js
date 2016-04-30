6193efcebdbf638261d0ee64e65c6a260affdd30
Filipp Gaponenko <psr1919plus21@gmail.com>
2016-04-30 17:06:17 +0300

    :rocket: Footer added.

--- markup/modules/contacts/contacts.js
+++ markup/modules/contacts/contacts.js
@@ -5,17 +5,23 @@ googleApi.load((google) => {
 });
 
 function initMap(google) {
-    let lascomOffice = {lat: 56.749565, lng: 37.226751};
+    let lascomOffice = {lat: 56.749665, lng: 37.22828};
 
     let mapDiv = document.getElementById('map');
     let map = new google.maps.Map(mapDiv, {
         center: lascomOffice,
-        zoom: 15
+        zoom: 16,
+        mapTypeControl: false
     });
+
 
     let marker = new google.maps.Marker({
         position: lascomOffice,
         map: map,
-        title: 'Lascom office'
+        labelClass: 'label'
     });
+
+    console.log(marker);
+
+
 }
