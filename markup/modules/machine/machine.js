f12488dd34fb0aff99e52772f7c2bf6e5c16df11
Filipp Gaponenko <psr1919plus21@gmail.com>
2016-05-03 01:08:32 +0300

    :rocket: Svg masks.

--- markup/modules/machine/machine.js
+++ markup/modules/machine/machine.js
@@ -16,9 +16,39 @@ $('#machine-layout').wbtRotator({
         background: '#161616'
     },
     masks: [{
-        id: 'Human Skull',
+        id: 'Рабочая поверхность',
         color: '#c4cb8a',
         src: 'static/img/plugins/machine/machine.{{01}}.svg'
+    },
+    {
+        id: 'Blend',
+        color: '#f44',
+        src: 'static/img/plugins/machine/machine-blend.{{01}}.svg'
+    },
+    {
+        id: 'Belts',
+        color: '#2f2',
+        src: 'static/img/plugins/machine/machine-belts.{{01}}.svg'
+    },
+    {
+        id: 'Cooling',
+        color: '#228',
+        src: 'static/img/plugins/machine/machine-cooling.{{01}}.svg'
+    },
+    {
+        id: 'Laser',
+        color: '#34ff86',
+        src: 'static/img/plugins/machine/machine-laser.{{01}}.svg'
+    },
+    {
+        id: 'Motors',
+        color: '#f44',
+        src: 'static/img/plugins/machine/machine-motors.{{01}}.svg'
+    },
+    {
+        id: 'Stops',
+        color: '#f44',
+        src: 'static/img/plugins/machine/machine-stops.{{01}}.svg'
     }]
 });
 
