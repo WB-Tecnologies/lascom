97112762ac1d5adac83ff13bbdba35cf50626061
Filipp Gaponenko <psr1919plus21@gmail.com>
2016-05-06 00:53:09 +0300

    :rocket: Menu highlight.

--- markup/modules/page-slider/page-slider.js
+++ markup/modules/page-slider/page-slider.js
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import $ from 'jquery';
 
 const SLIDE_BEFORE_PARALAX = 2;
 const SLIDE_PARALAX = 3;
@@ -10,13 +11,16 @@ let viewPortHeight = Math.max(document.documentElement.clientHeight, window.inne
 let currentSlide = 1;
 let pageYOld = window.pageYOffset;
 let getSlideOverlayMemo = _.memoize(getSlideOverlay);
+let menuIndexElements = $('.header-nav-list__index .header-nav-list_link');
+let menuMachineElements = $('.header-nav-list__machine .header-nav-list_link');
+let currentMenu;
 
 initialize();
 
 function initialize() {
+    updateMenuInit();
     initSliderHeight();
-    // document.querySelector('body').style.height = getCurentSlideBoundary(slides.length) + 'px';
-    // scrollSlides();
+    scrollSlides();
     window.addEventListener('scroll', scrollSlidesThrottled);
 }
 
@@ -48,8 +52,8 @@ function сurentSlideBoundaryFactory() {
     };
 }
 
-function getSlideOpacity(_pageYNew, _currentSlide) {
-    return _pageYNew / viewPortHeight - (_currentSlide - 1);
+function getSlideOpacity(_pageYNew, _currentSlide, t) {
+    return (_pageYNew - t) / viewPortHeight - (_currentSlide - 1);
 }
 
 function getSlideOverlay(element) {
@@ -59,7 +63,7 @@ function getSlideOverlay(element) {
 function updateSlideOpacity(slide, slideOpacity) {
     slide.style.opacity = slideOpacity;
 
-    if (slideOpacity < 0.1) {
+    if (slideOpacity < 0.05) {
         slide.classList.add('detached-screen_overlay__transparent');
     } else {
         slide.classList.remove('detached-screen_overlay__transparent');
@@ -80,14 +84,87 @@ function updateParalaxPosition(_currentSlide, scrolledSlice) {
     }
 }
 
+function updateMenuInit() {
+    if (menuIndexElements.length) {
+        currentMenu = 'index';
+    } else if (menuMachineElements.length) {
+        currentMenu = 'machine';
+    }
+}
+
+function updateIndexMenu() {
+
+    if (currentSlide === 4 || pageYOld === 0) {
+        menuIndexElements.removeClass('header-nav-list_link__active');
+    } else if (currentSlide === 5) {
+        menuIndexElements.removeClass('header-nav-list_link__active');
+        menuIndexElements[currentSlide - 2].classList.add('header-nav-list_link__active');
+    } else {
+        menuIndexElements.removeClass('header-nav-list_link__active');
+        menuIndexElements[currentSlide - 1].classList.add('header-nav-list_link__active');
+    }
+
+}
+
+function updateMachineMenu() {
+
+
+
+    switch (currentSlide) {
+        case 3:
+            menuMachineElements.removeClass('header-nav-list_link__active');
+            menuMachineElements[currentSlide - 1].classList.add('header-nav-list_link__active');
+            break;
+        case 4:
+            menuMachineElements.removeClass('header-nav-list_link__active');
+            menuMachineElements[currentSlide - 1].classList.add('header-nav-list_link__active');
+            break;
+        case 5:
+            menuMachineElements.removeClass('header-nav-list_link__active');
+            menuMachineElements[currentSlide - 1].classList.add('header-nav-list_link__active');
+            break;
+        case 6:
+            menuMachineElements.removeClass('header-nav-list_link__active');
+            menuMachineElements[currentSlide - 1].classList.add('header-nav-list_link__active');
+            break;
+        case 7:
+            menuMachineElements.removeClass('header-nav-list_link__active');
+            break;
+        case 8:
+            menuMachineElements.removeClass('header-nav-list_link__active');
+            menuMachineElements[currentSlide - 2].classList.add('header-nav-list_link__active');
+            break;
+
+        default:
+            menuMachineElements.removeClass('header-nav-list_link__active');
+            menuMachineElements[currentSlide].classList.add('header-nav-list_link__active');
+    }
+
+    if (pageYOld === 0) {
+        menuMachineElements.removeClass('header-nav-list_link__active');
+        menuMachineElements[currentSlide - 1].classList.add('header-nav-list_link__active');
+    }
+
+
+}
+
+function updateMenu() {
+    if (currentMenu === 'index') {
+        updateIndexMenu();
+    } else if (currentMenu === 'machine') {
+        updateMachineMenu();
+    }
+}
+
 function scrollSlides() {
     let currentSlideBoundary = getCurentSlideBoundary(currentSlide);
     let heightDelta = slides[currentSlide].clientHeight - viewPortHeight;
+    let heightDeltaPrev = slides[currentSlide - 1].clientHeight - viewPortHeight;
     let pageYNew = window.pageYOffset;
 
     // Scroll direction top.
     if (pageYOld > pageYNew) {
-        let slideOpacity = getSlideOpacity(pageYNew, currentSlide);
+        let slideOpacity = getSlideOpacity(pageYNew, currentSlide, heightDeltaPrev);
         let slideOverlay = getSlideOverlayMemo(slides[currentSlide - 1]);
         updateSlideOpacity(slideOverlay, slideOpacity);
         updateParalaxPosition(currentSlide, slideOpacity);
@@ -105,7 +182,7 @@ function scrollSlides() {
 
     // Scroll direction bottom.
     } else {
-        let slideOpacity = getSlideOpacity(pageYNew, currentSlide);
+        let slideOpacity = getSlideOpacity(pageYNew, currentSlide, heightDeltaPrev);
         let slideOverlay = getSlideOverlayMemo(slides[currentSlide - 1]);
         updateSlideOpacity(slideOverlay, slideOpacity);
         updateParalaxPosition(currentSlide, slideOpacity);
@@ -119,4 +196,5 @@ function scrollSlides() {
     }
 
     pageYOld = pageYNew;
+    updateMenu();
 }
