e5c97785f44240f304a63fd779005678841c273b
Filipp Gaponenko <psr1919plus21@gmail.com>
2016-05-06 04:30:23 +0300

    :wrench: Remove textnodes from table.

--- markup/static/js/main.js
+++ markup/static/js/main.js
@@ -4,3 +4,4 @@ import * as contacts from 'modules/contacts/contacts';
 import * as slider from 'modules/page-slider/page-slider';
 import * as machineLayout from 'modules/machine/machine';
 import * as laser from 'modules/laser/laser';
+import * as removeTextNodes from 'modules/kit-variants/kit-variants';
