2af946584f5633a030a0fd41b86a742711ac48a9
Filipp Gaponenko <psr1919plus21@gmail.com>
2016-05-06 04:35:12 +0300

    :necktie: Fix table col..

--- markup/modules/kit-variants/kit-variants.js
+++ markup/modules/kit-variants/kit-variants.js
@@ -1,9 +1,1 @@
-let $tableContent = $('.kit-table_row');
 
-removeTextNodes();
-
-function removeTextNodes() {
-    $tableContent.contents().filter(() => {
-        return this.nodeType === 3;
-    }).remove();
-}
