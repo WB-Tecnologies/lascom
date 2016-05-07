78d83ad6cba87b4f92517ae0213a56da753776f2
Filipp Gaponenko <psr1919plus21@gmail.com>
2016-05-07 22:23:24 +0300

    :rocket: Manual scroll paralax for mobile size.

--- markup/static/js/main.js
+++ markup/static/js/main.js
@@ -2,6 +2,7 @@ import * as header from 'modules/header/header';
 import * as machineHeader from 'modules/machine-header/machine-header';
 import * as contacts from 'modules/contacts/contacts';
 import * as slider from 'modules/page-slider/page-slider';
+import * as manualParalax from 'modules/usage-scope/usage-scope';
 import * as machineLayout from 'modules/machine/machine';
 import * as laser from 'modules/laser/laser';
 import * as orderValidator from 'modules/order-form/order-form';
