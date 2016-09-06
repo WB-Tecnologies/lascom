c77e51806264b70a99ed07d5cddc75c8a26cbb04
Filipp Gaponenko <psr1919plus21@gmail.com>
2016-09-06 10:43:42 +0300

    :rocket: Include footer on about page.

--- markup/modules/footer/footer.js
+++ markup/modules/footer/footer.js
@@ -1,8 +1,10 @@
+console.log('here');
 const $body = $('body');
 const scrollTopBtn = $('.scrolltop-btn');
 
 scrollTopBtn.on('click', scrollTop);
 
 function scrollTop() {
+    console.log('dsds');
     $body.animate({'scrollTop': 0}, 800);
 }
