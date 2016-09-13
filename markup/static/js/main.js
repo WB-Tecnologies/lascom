d07b02471a9e877649ece1db82351fc4984ba303
Filipp Gaponenko <psr1919plus21@gmail.com>
2016-09-13 23:03:07 +0300

    :wrench: Replace machine with ulsp.

--- markup/static/js/main.js
+++ markup/static/js/main.js
@@ -1,8 +1,8 @@
 import * as header from 'modules/header/header';
-import * as machineHeader from 'modules/machine-header/machine-header';
+import * as ulspHeader from 'modules/ulsp-header/ulsp-header';
 import * as slider from 'modules/page-slider/page-slider';
 import * as manualParalax from 'modules/usage-scope/usage-scope';
-import * as machineLayout from 'modules/machine/machine';
+import * as machineLayout from 'modules/ulsp/ulsp';
 import * as laser from 'modules/laser/laser';
 import * as orderValidator from 'modules/order-form/order-form';
 import * as samplesValidator from 'modules/get-samples/get-samples';
