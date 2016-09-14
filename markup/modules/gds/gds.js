074f4ad736c0601033bf6b1392a7600e096aeb9f
Filipp Gaponenko <psr1919plus21@gmail.com>
2016-09-14 11:19:09 +0300

    :wrench: Fix multiple l10n.

--- markup/modules/gds/gds.js
+++
@@ -1,1 +0,0 @@
-import * as ulspl10n from 'static/js/plugins/gds.l10n.js';
--- markup/modules/ulsp/ulsp.js
+++ markup/modules/ulsp/ulsp.js
@@ -1,3 +1,2 @@
 import * as formStyler from 'static/js/plugins/formStyler.js';
 import * as rotator from 'static/js/plugins/rotator.js';
-import * as ulspl10n from 'static/js/plugins/ulsp.l10n.js';
