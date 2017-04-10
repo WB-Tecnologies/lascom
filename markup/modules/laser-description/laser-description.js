e79c3d9b28f8a118b8d28b22bd571f8aad0ef8cd
Filipp Gaponenko <psr1919plus21@gmail.com>
2017-04-10 16:58:03 +0300

    :rocket: Toggle zoomCover by mediaquery.

--- markup/modules/laser-description/laser-description.js
+++ markup/modules/laser-description/laser-description.js
@@ -1,7 +1,37 @@
 import '@zeitiger/elevatezoom';
 
-$('.c-zoom-cover').elevateZoom({
-    zoomType: 'lens',
-    lensShape: 'round',
-    lensSize: 300
-});
+let $zoomCover = $('.c-zoom-cover');
+
+toggleZoomCoverByMediaQuery();
+
+function zoomCoverInit() {
+    $zoomCover.elevateZoom({
+        zoomType: 'lens',
+        lensShape: 'round',
+        lensSize: 300
+    });
+}
+
+function zoomCoverDestroy() {
+    $.removeData($zoomCover, 'elevateZoom');
+    $('.zoomContainer').remove();
+}
+
+function toggleZoomCoverByMediaQuery() {
+    var _matchMediaObject = window.matchMedia('(max-width: 1024px)');
+
+    zoomCoverToggle(_matchMediaObject.matches);
+
+    // Add listener.
+    _matchMediaObject.addListener(function () {
+        zoomCoverToggle(_matchMediaObject.matches);
+    });
+}
+
+function zoomCoverToggle(_mediaMatch) {
+    if (_mediaMatch) {
+        zoomCoverDestroy();
+    } else {
+        zoomCoverInit();
+    }
+}
