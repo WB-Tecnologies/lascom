bf21f10f666314c3e4ff9d27006f002265cf7417
Filipp Gaponenko <psr1919plus21@gmail.com>
2017-04-12 00:59:09 +0300

    :rocket: Meta.

--- markup/modules/head/data/data.js
+++ markup/modules/head/data/data.js
@@ -1,7 +1,62 @@
 head: {
-    defaults: {
-        title: 'Lascom — эффективная обработка стекла лазером.',
-        description: 'Ласком создает оборудование для обработки стекла лазером. Оборудование Ласком используют для финишной обработки окон и другого стекла в объектах недвижимости, транспорта, интерьеров и экстерьеров.',
+    index: {
+        title: 'Lascom',
+        description: 'Lascom designs both equipment and laser technologies for glass processing. All together it completely changes the traditional perception of the qualities of this material. Lascom machinery and technologies are used and applied for architectural glazing, interiors, transport and other industries.',
+        useSocialMetaTags: true
+    },
+    index_RU: {
+        title: 'Lascom',
+        description: 'Ласком создает оборудование и разрабатывает технологии для обработки стекла лазером, меняя привычное представление о возможностях этого материала. Оборудование «Ласком» используются для финишной обработки стекла в объектах недвижимости, транспорта, интерьеров и экстерьеров.',
+        useSocialMetaTags: true
+    },
+    ulsp: {
+        title: 'Lascom | ULSP',
+        description: 'Coating evaporation laser table (ULSP) removes electric conductive coating from the surface of low-emissivity glass with a speed of 360 mm²/sec. Software supplied together with the table, controls temperature values in each spot of glass of any shape.',
+        useSocialMetaTags: true
+    },
+    ulsp_RU: {
+        title: 'Lascom | ULSP',
+        description: 'Установка лазерного снятия покрытия (УЛСП) удаляет участки токопроводящего покрытия с низкоэмиссионного стекла со скоростью 2 кв.м. в минуту.',
+        useSocialMetaTags: true
+    },
+    gds: {
+        title: 'Lascom | GDS',
+        description: 'The cold-spraying machine allows to automate the process of the busbar application onto the glass, decreasing the cost and labor required for it.',
+        useSocialMetaTags: true
+    },
+    gds_RU: {
+        title: 'Lascom | GDS',
+        description: 'Установка напыления шин позволяет автоматизировать процесс нанесения токопроводящей шины на электрообогреваемое стекло, значительно сократив трудоёмкость и стоимость этого процесса.',
+        useSocialMetaTags: true
+    },
+    mlc: {
+        title: 'Lascom | MLC',
+        description: 'Module for laser cutting (MLC) is intended for the cutting of the so called overblow from the hollow glass workpiece — a cognac or wine glass, «martini» glass and other types of tableware.',
+        useSocialMetaTags: true
+    },
+    mlc_RU: {
+        title: 'Lascom | MLC',
+        description: 'Модуль для лазерной резки (MLC) предназначен для отрезания технологического колпака от заготовки выдувного изделия — коньячного, винного бокала, бокала типа «мартини» и прочих видов сортовой посуды.',
+        useSocialMetaTags: true
+    },
+    about: {
+        title: 'Lascom | Company',
+        description: 'Lascom designs both equipment and laser technologies for glass processing. All together it completely changes the traditional perception of the qualities of this material. Lascom machinery and technologies are used and applied for architectural glazing, interiors, transport and other industries.',
+        useSocialMetaTags: true
+    },
+    about_RU: {
+        title: 'Lascom | Компания',
+        description: 'Ласком создает оборудование и разрабатывает технологии для обработки стекла лазером, меняя привычное представление о возможностях этого материала. Оборудование «Ласком» используются для финишной обработки стекла в объектах недвижимости, транспорта, интерьеров и экстерьеров.',
+        useSocialMetaTags: true
+    },
+    contacts: {
+        title: 'Lascom | Contacts',
+        description: 'Lascom designs both equipment and laser technologies for glass processing. All together it completely changes the traditional perception of the qualities of this material. Lascom machinery and technologies are used and applied for architectural glazing, interiors, transport and other industries.',
+        useSocialMetaTags: true
+    },
+    contacts_RU: {
+        title: 'Lascom | Контакты',
+        description: 'Ласком создает оборудование и разрабатывает технологии для обработки стекла лазером, меняя привычное представление о возможностях этого материала. Оборудование «Ласком» используются для финишной обработки стекла в объектах недвижимости, транспорта, интерьеров и экстерьеров.',
         useSocialMetaTags: true
     }
 }
